2d458e01bb5ecba5b830733ff4b76598d27117de
lavinog <greglavino+github@gmail.com>
2018-11-23 02:24:04 -0800

    Update stop_server.sh
    Added system messages and saveworld command before exiting.

--- bin/stop_server.sh
+++ bin/stop_server.sh
@@ -15,5 +15,11 @@ fi
 source "${ETC_CONF_FILE}"
 source "${CONF_GAME_FILE_FUNCTION_LIB}"
 
+telnet_command "say System shutdown request occured."
+telnet_command "say The server will be shutting down in 10 seconds."
+sleep 10
+telnet_command "saveworld"
+telnet_command "say Shutting down server now."
+sleep 1
 telnet_command "shutdown"
 sleep 3
